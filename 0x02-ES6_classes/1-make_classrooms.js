93e8346f941a7a51453a90bffcbdaafa52419c22
LeadConsult <106965102+LeadConsult@users.noreply.github.com>
2023-04-20 21:05:10 +0100

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,9 +1,14 @@
 import ClassRoom from './0-classroom';
 
-export default function initializeRooms() {
-  const class1 = new ClassRoom(19);
-  const class2 = new ClassRoom(20);
-  const class3 = new ClassRoom(34);
-  const newList = [class1, class2, class3];
-  return (newList);
+const initializeRooms = () => {
+  const maxStudentsSize = [19, 20, 34];
+  const newList = maxStudentsSize.map((size) => new ClassRoom(size));
+  return newList;
 }
+
+export default initializeRooms;
+// This function creates an array of class instances with the provided 
+// maximum student size using the map function of arrays in ES6.
+
+// It uses an arrow function and the const keyword to declare the 
+// function and variables, respectively.
