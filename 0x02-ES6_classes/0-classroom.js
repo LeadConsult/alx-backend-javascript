62781e18c05359aeb89689074690312602a3188a
LeadConsult <leadconsultnig@gmail.com>
2023-04-20 20:54:31 +0100

    ES6_classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,9 @@
+// Define a ClassRoom class that takes a maximum student size
+class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
+
+// Export the ClassRoom class as the default export
+export default ClassRoom;
