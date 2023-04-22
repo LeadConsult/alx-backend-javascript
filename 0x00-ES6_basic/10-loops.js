4fec5029dee8df5ac496d4f8267bebfb7b3a5e8f
LeadConsult <leadconsultnig@gmail.com>
2023-04-22 01:56:18 +0100

    modified:   0x00-ES6_basic/10-loops.js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,9 +1,15 @@
+/**
+ * Takes an array of strings and an appending string and returns a new array
+ * where each element of the original array is concatenated with the appending string.
+ * @param {array} array - The array to modify.
+ * @param {string} appendString - The string to append to each element.
+ * @returns {array} - A new array with the modified elements.
+ */
+
 export default function appendToEachArrayValue(array, appendString) {
-  array = []
-    for (const idx of array) {
-      let value = array[idx];
-      array[idx] = appendString + value;
-    }
-  
-    return array;
+  const newArray = [];
+  for (const idx of array) {
+    newArray.push(appendString + idx);
   }
+  return newArray;
+}
