311d6dcce1bdfdec4ae18647636279eba1d2b440
LeadConsult <106965102+LeadConsult@users.noreply.github.com>
2023-04-22 02:10:22 +0100

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,8 +1,6 @@
-/**
- * Creates an object that includes all employees and the number of departments.
- * @param {object} employeesList - An object with department names as keys and arrays of employees as values.
- * @returns {object} - An object that includes all employees and the number of departments.
- */
+// Creates an object that includes all employees and the number of departments.
+// @param {object} employeesList - An object with department names as keys and arrays of employees as values.
+// @returns {object} - An object that includes all employees and the number of departments.
 
 export default function createReportObject(employeesList) {
   const allEmployees = Object.values(employeesList).flat();
