883434c1b31ba18875937181119f0f0ac7bc675a
LeadConsult <106965102+LeadConsult@users.noreply.github.com>
2023-04-27 02:22:00 +0100

    Update main.ts

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,34 +1,24 @@
-// task_5 Advanced types Part 1
 interface DirectorInterface {
   workFromHome(): string
-
   getCoffeeBreak(): string
-  
   workDirectorTasks(): string
-
 }
 
 interface TeacherInterface {
   workFromHome(): string
-
   getCoffeeBreak(): string
-
   workTeacherTasks(): string
 }
 
 export class Director implements DirectorInterface {
   workFromHome = (): string => "Working from home";
-
   getCoffeeBreak = (): string => "Getting a coffee break";
-
   workDirectorTasks = (): string => "Getting to director tasks";
 }
 
 export class Teacher implements TeacherInterface {
   workFromHome = (): string => "Cannot work from home";
-
   getCoffeeBreak = (): string => "Cannot have a break";
-
   workTeacherTasks = (): string => "Getting to work";
 }
 
