9adb713942af12175b01205f4496fa9586198f42
LeadConsult <leadconsultnig@gmail.com>
2023-06-14 01:56:44 +0100

    edt

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,48 +1,46 @@
 const fs = require('fs');
 
 function countStudents(fileName) {
-  const students = {};  // Object to store students in each field
-  const fields = {};    // Object to store count of students in each field
-  let length = 0;       // Variable to store the total number of students
+  const students = {}; // Object to store students by field
+  const fields = {}; // Object to store field counts
+  let length = 0; // Total number of students
 
   try {
-    const read_file = fs.readFileSync(fileName, 'utf-8');  
     // Read the file synchronously
-    const to_lines = read_file.toString().split('\n');         
-    // Split the read_file into to_lines
+    const fileContents = fs.readFileSync(fileName, 'utf-8'); 
+    const lines = fileContents.toString().split('\n'); 
+    // Split the file contents into lines
 
-    for (let i = 0; i < to_lines.length; i += 1) {
-      if (to_lines[i]) {
-        length += 1;
-        const field = to_lines[i].toString().split(',');     
+    for (let i = 0; i < lines.length; i += 1) {
+      if (lines[i]) {
+        length += 1; // Increment the total number of students
+
+        const field = lines[i].toString().split(',');
         // Split the line into fields
-
-        // Update the students object with the student's name under
-        // the corresponding field
         if (Object.prototype.hasOwnProperty.call(students, field[3])) {
-          students[field[3]].push(field[0]);
+          // Check if the field already exists in the students object
+          students[field[3]].push(field[0]); 
+          // Add the student to the existing field
         } else {
           students[field[3]] = [field[0]];
+          // Create a new field and add the student
         }
 
-        // Update the fields object by incrementing the count for
-        // the corresponding field
         if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
-          fields[field[3]] += 1;
+          // Check if the field already exists in the fields object
+          fields[field[3]] += 1; // Increment the field count
         } else {
-          fields[field[3]] = 1;
+          fields[field[3]] = 1; // Create a new field and set the count to 1
         }
       }
     }
 
     const l = length - 1;
+    // Subtract 1 from the length to exclude the header line
     console.log(`Number of students: ${l}`);
-
-    // Output the count and list of students for each field
     for (const [key, value] of Object.entries(fields)) {
       if (key !== 'field') {
-        console.log(`Number of students in ${key}: ${value}. List: \
-        ${students[key].join(', ')}`);
+        console.log(`Number of students in ${key}: ${value}. List: ${students[key].join(', ')}`);
       }
     }
   } catch (error) {
