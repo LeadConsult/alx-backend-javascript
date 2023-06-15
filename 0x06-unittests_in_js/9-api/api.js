5301a0379b08bda3e999be8305af5128ec6e3b81
LeadConsult <leadconsultnig@gmail.com>
2023-06-15 22:12:34 +0100

    9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,15 +1,32 @@
 const express = require('express');
-
-// Create an instance of express called app
 const app = express();
 
-// Listen to port 7865
+// Other existing code...
+
+app.get('/cart/:id', (req, res) => {
+  const id = req.params.id;
+
+  // Validate if id is a number
+  if (isNaN(id)) {
+    return res.status(400).json({ error: 'Invalid cart ID. Please provide a valid number.' });
+  }
+
+  // Replace the following line with your actual logic to retrieve payment methods for the specified cart ID
+  const paymentMethods = getPaymentMethods(id);
+
+  res.json({ paymentMethods: paymentMethods });
+});
+
+// Other existing code...
+
 app.listen(7865, () => {
   console.log('API available on localhost port 7865');
 });
 
-// Route GET /
-app.get('/', (req, res) => {
-  // Return the message "Welcome to the payment system"
-  res.send('Welcome to the payment system');
-});
+// Helper function to retrieve payment methods for a cart ID
+function getPaymentMethods(cartId) {
+  // Replace this with your actual logic to fetch payment methods for the specified cart ID
+  return ['Credit Card', 'Debit Card', 'PayPal'];
+}
+
+module.exports = app;
